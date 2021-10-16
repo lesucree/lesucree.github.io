1045b2039100e34833369c2218e63ba368fb1c22
lesucree <92579465+lesucree@users.noreply.github.com>
2021-10-16 10:35:20 +0800

    added all the anchor links and smooth scrolls

--- js/navbar.js
+++ js/navbar.js
@@ -29,3 +29,13 @@ navSlide();
 $(".nav-links li").click(function () {
   $(".burger").click();
 });
+
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
